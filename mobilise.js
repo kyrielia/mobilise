a43041a73ea904b429323bc7961f60420caa4710
eliak <Kyriacos.Elia@bskyb.com>
2013-04-12 09:28:35 +0100

    Element is given tel tag irrespective of browser type. Added jquery and html file for testing purposes.

--- mobilise.js
+++ mobilise.js
@@ -12,15 +12,26 @@
 	
 	Mobilise.prototype = {
 		init: function() {
-			// will insert stuff here
+			if (this.isMobileBrowser()) {
+				var element = $(this.element);
+		
+				var phoneNumber = element.text();
+				noSpacesNumber = phoneNumber.replace(/\s+/g, "");
+			
+				element.html("<a href=\"tel:" + noSpacesNumber + "\">" + phoneNumber + "</a>");	
+			}
+		},
+		
+		isMobileBrowser: function() {
+			return true;
 		}
 	};
 	
 	$.fn[pluginName] = function(options) {
-		return this.each(function() {
-			if (!$.data(this, "plugin_" + pluginName))
-				$.data(this, "plugin_" + pluginName, new Mobilise(this, options));
-			}
-		});
-	};
+        return this.each(function() {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Mobilise(this, options));
+            }
+        });
+    };
 })(jQuery, window, document);
